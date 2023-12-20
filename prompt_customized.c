904de7a50d143222e5298e9c1781bc1b3c3571d4
IBlancI <adilslimani93130@gmail.com>
2023-12-20 09:50:31 -0800

    Betty fixed

--- prompt_customized.c
+++ prompt_customized.c
@@ -1,8 +1,7 @@
 #include "shell.h"
 
-/*
- * Prompt - Print the environment variables.
- *
+/**
+ * printPrompt - Print the environment variables.
  * This function prints the environment variables to the standard output.
  * Each environment variable is printed on a new line.
  * Return: no return, void function
