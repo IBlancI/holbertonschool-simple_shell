d4dc1690b9a680d7acc34ae7f99cba7bf087c7ce
IBlancI <adilslimani93130@gmail.com>
2023-12-21 06:31:04 -0800

    Commentary added to the file prompt_customized.c

--- prompt_customized.c
+++ prompt_customized.c
@@ -7,12 +7,17 @@
  * Return: no return, void function
  */
 
+
+/* This function prints all environment variables of the program */
 void printPrompt(void)
 {
+	/* Declaration of a pointer to an array of strings */
 	char **env;
 
+	/* Loop through the environment array until the end (NULL) is reached */
 	for (env = environ; *env != NULL; env++)
 	{
+		/* Print each environment variable on a new line */
 		printf("%s\n", *env);
 	}
 }
