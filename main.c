e17afb1dda3c57fd6c244391fca68c191fde64b0
IBlancI <adilslimani93130@gmail.com>
2023-12-21 07:28:10 -0800

    Commentary added to the main.c file

--- main.c
+++ main.c
@@ -12,26 +12,33 @@ int main(void)
 	size_t len_input = 0;
 	ssize_t read = 0;
 
+	/* Continuously read user input */
 	while (1)
 	{
+		/* If input is from a terminal, print a prompt */
 		if (isatty(STDIN_FILENO))
 		{
-			printf("LOCACA ");
+			printf("Prompt_Adil_Saber ");
 			fflush(stdout);
 		}
 
+		/* Read user input */
 		read = getline(&input, &len_input, stdin);
+		/* Check for EOF */
 		if (read == EOF)
 		{
 			free(input);
 			exit(0);
 		}
+		/* Remove newline character from input */
 		if (read > 0 && input[read - 1] == '\n')
 		{
 			input[read - 1] = '\0';
 		}
+		/* Tokenize and process input */
 		token_input(input);
 	}
+	/* Free allocated memory */
 	free(input);
 	return (0);
 }
